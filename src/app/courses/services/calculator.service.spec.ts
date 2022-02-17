bde15300e5e5d5c2bf755ba106e8fefa03ee009c
Pantelis Papasavvas <pantpap@gmail.com>
2022-02-17 10:37:23 +0200

    initialize project from course and add calculator.service

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,24 @@
+import {CalculatorService} from './calculator.service';
+import {LoggerService} from './logger.service';
+
+describe('CalculatorService', () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
+  it('should add two numbers', () => {
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subtract two numbers', () => {
+    const result = calculator.subtract(2, 2);
+    expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+});
