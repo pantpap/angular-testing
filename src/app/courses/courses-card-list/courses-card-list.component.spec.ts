0b7b5582fd7b02f3c1af9c6e6bb617e1ffea45aa
Pantelis Papasavvas <pantpap@gmail.com>
2022-02-23 15:31:13 +0200

    testing: (course-card-list) prepare beforeEach waitForAsync

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -16,18 +16,18 @@ describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
 
-beforeEach(() => {
+beforeEach(waitForAsync(() => {
   TestBed.configureTestingModule({
     imports: [CoursesModule]
   }).compileComponents().then(() => {
     fixture = TestBed.createComponent(CoursesCardListComponent);
     component = fixture.componentInstance;
   });
-});
+}));
 
   it('should create the component', () => {
-
-   pending();
+    expect(component).toBeTruthy();
+    console.log(component);
 
   });
 
