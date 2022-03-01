a28f2d152d7ed34d22f893afd5dca1b6fc08195d
Pantelis Papasavvas <pantpap@gmail.com>
2022-03-01 10:54:42 +0200

    testing: (homeComponent) configure TestBed

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
 
@@ -13,49 +13,55 @@ import {of} from 'rxjs';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {click} from '../common/test-utils';
 
-
-
-
 describe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
-  let component:HomeComponent;
+  let component: HomeComponent;
   let el: DebugElement;
 
-  beforeEach((() => {
-
-
+  beforeEach(waitForAsync (() => {
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
+    TestBed.configureTestingModule({
+      imports: [CoursesModule, NoopAnimationsModule],
+      providers: [{
+        provide: CoursesService, useValue: coursesServiceSpy
+      }]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(HomeComponent);
+      component = fixture.componentInstance;
+      el = fixture.debugElement;
+    });
   }));
 
-  it("should create the component", () => {
+  it('should create the component', () => {
 
     expect(component).toBeTruthy();
 
   });
 
 
-  it("should display only beginner courses", () => {
+  it('should display only beginner courses', () => {
 
     pending();
 
   });
 
 
-  it("should display only advanced courses", () => {
+  it('should display only advanced courses', () => {
 
       pending();
 
   });
 
 
-  it("should display both tabs", () => {
+  it('should display both tabs', () => {
 
     pending();
 
   });
 
 
-  it("should display advanced courses when tab clicked", () => {
+  it('should display advanced courses when tab clicked', () => {
 
     pending();
 
