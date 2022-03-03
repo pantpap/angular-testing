82558b4a4a76ae0151e84c86a66b41215188ca0b
Pantelis Papasavvas <pantpap@gmail.com>
2022-03-03 15:45:42 +0200

    testing: (homeComponent) execute all async assertions outside setTimeout after using flush()

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -71,14 +71,17 @@ describe('HomeComponent', () => {
     const tabs = el.queryAll(By.css('.mat-tab-label'));
     click(tabs[1]);
     fixture.detectChanges();
+    let cardTitles;
     setTimeout(() => {
-      const cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
+      cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
 
-      expect(cardTitles.length).toBeGreaterThan(0, 'Card titles must be exist');
-      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+      // expect(cardTitles.length).toBeGreaterThan(0, 'Card titles must be exist');
+      // expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
     }, 500);
 
     flush();
+    expect(cardTitles.length).toBeGreaterThan(0, 'Card titles must be exist');
+    expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
   }));
 });
 
